a78d0f4b03b5273963c336118d03adab83db2af4
CI-Server <ci@example.com>
2015-09-25 21:16:24 -0700

    Make colored buttons work against most backgrounds

--- src/layout-test.js
+++ src/layout-test.js
@@ -2,18 +2,25 @@ import React, {Component} from 'react';
 import d3 from 'd3-color';
 import {Layout} from './Layout.js';
 
+window.d3 = d3;
+
 var sunsetScale = d3.interpolateHcl('#f2f19c', '#282c9c');
 
 var themeColorScales = {
+  bw: {
+    interpolator: d3.interpolateHsl,
+    start:  '#010101',
+    end: '#fefefe'
+  },
+  radioactive: {
+    interpolator: d3.interpolateHcl,
+    start: '#545561',
+    end: '#EAFB5F'
+  },
   terminal: {
     interpolator: d3.interpolateHsl,
-    start: '#0ef08b',
-    end:  'black'
-  },
-  mochaInverted: {
-    interpolator: d3.interpolateHcl,
-    start: '#f2f0e7',
-    end:  '#4d2f34'
+    start:  'black',
+    end: '#0ef08b'
   },
   mocha: {
     interpolator: d3.interpolateCubehelix,
@@ -78,6 +85,7 @@ class App extends Component {
     super(props);
     let theme = themeColorScales.mocha;
     this.state = {
+      invert: false,
       interpolator: theme.interpolator,
       startColor: theme.start,
       endColor: theme.end
@@ -109,14 +117,54 @@ class App extends Component {
   //   toMatchColor = d3.hcl(toMatchColor);
   //   return d3.hcl(fromColor.h, toMatchColor.c, toMatchColor.l);
   // }
-  rotateColorToMatch (fromColor, toMatchColor) {
-    fromColor = d3.cubehelix(fromColor);
-    toMatchColor = d3.cubehelix(toMatchColor);
-    return d3.cubehelix(fromColor.h, toMatchColor.s, toMatchColor.l);
+  rotateColorToMatch (fromColor, scaleAmount) {
+    let luminosityDiffThreshold = 0;
+    let clipThreshold = luminosityDiffThreshold;
+
+
+    function clip (l) {
+      var noLowerThanLuminsityThreshold = Math.max(clipThreshold, l);
+      var max = 100 - clipThreshold;
+      var andNoHigherThanMax = Math.min(max, noLowerThanLuminsityThreshold);
+      return andNoHigherThanMax;
+    }
+    fromColor = d3.hcl(fromColor);
+    let themeScale = this.colorer();
+
+    // Midpoint of luminosity between the two colors;
+    let midL = (d3.hcl(themeScale(0)).l + d3.hcl(themeScale(1)).l) / 2;
+    let toMatchColor = d3.hcl(themeScale(scaleAmount));
+    // let chroma = (fromColor.c + toMatchColor.c + d3.hcl(themeScale(0)).c) / 3;
+    let chroma = (d3.hcl(themeScale(0)).c + d3.hcl(themeScale(1)).c + Math.min(d3.hcl(themeScale(0)).c, d3.hcl(themeScale(1)).c)) / 3;
+    if (Math.abs(chroma - toMatchColor.c) < 20) chroma = Math.max(toMatchColor.c + 30, 0);
+    // chroma = toMatchColor.c + 50;
+    // let chroma = 30;
+    // chroma = 0;
+
+    // let luminosity = 70;
+
+    // Maximum difference
+    let maxDiffluminosity = toMatchColor.l < midL ? clip(100) : clip(0);
+
+    // Minimal difference
+    let minDiffLuminosity = clip(
+      toMatchColor.l < 50 ?
+        luminosityDiffThreshold + toMatchColor.l
+        : toMatchColor.l - luminosityDiffThreshold
+    );
+
+    let luminosity = maxDiffluminosity*.6 + minDiffLuminosity*.4;
+
+    let color =  d3.hcl(fromColor.h, chroma, luminosity);
+    let colorDifference = color.l - toMatchColor.l;
+    if (Math.abs(colorDifference)  < 25) {
+      return 'black';
+    }
+    return color;
   }
   render () {
     let sizes = [];
-    let times = 10;
+    let times = 11;
     for (var i = 0; i < times; i++) { sizes.push(i); }
 
     var style = this.styler();
@@ -125,19 +173,65 @@ class App extends Component {
     var invertedThemeScale = this.colorer({invert: true});
     // sunsetScale = themeScale;
 
-    let middleColor = themeScale(0.333);
-    let solidColoredButtonStyle = (color) => style.btn({
-      solid: true,
-      themeScale: d3.interpolateHcl(
-        this.rotateColorToMatch(color, middleColor),
-        this.state.endColor
-      )
-    });
+    // console.log(d3.hcl('white').l, d3.hcl('black').l);
+    let startColorLuminosity = d3.hcl(this.state.startColor).l;
+    let endColorLuminosity = d3.hcl(this.state.endColor).l;
+    let favorStartColor = startColorLuminosity > endColorLuminosity;
+    if (favorStartColor) {
+      console.log(startColorLuminosity);
+    }
 
-    let blueStyle = solidColoredButtonStyle('blue');
-    let redStyle = solidColoredButtonStyle('red');
-    let greenStyle = solidColoredButtonStyle('green');
-    let yellowStyle = solidColoredButtonStyle('yellow');
+    // let solidColoredButtonStyle = (color) => {
+    //   let middleColor = themeScale(0.2);
+    //   return style.btn({
+    //     solid: true,
+    //     themeScale: d3.interpolateHcl(
+    //       this.rotateColorToMatch(color, middleColor),
+    //       this.state.endColor
+    //     )
+    //   })
+    // };
+
+    let solidColoredButtonStyle = (color, bgScaleAmount) => {
+      let middleColor = this.rotateColorToMatch(color, bgScaleAmount);
+      return style.btn({
+        solid: true,
+        themeScale: d3.interpolateHcl(middleColor, themeScale(bgScaleAmount))
+      })
+    };
+
+    // let solidColoredButtonStyle = (color) => {
+    //   let middleColor = themeScale(0.8);
+    //   return style.btn({
+    //     solid: true,
+    //     themeScale: d3.interpolateHcl(
+    //       this.rotateColorToMatch(color, middleColor),
+    //       this.state.startColor
+    //     )
+    //   })
+    // };
+
+    let colors = [
+      // 'black',
+      // 'gray',
+      // 'white',
+      '#0088BF',
+      '#C40233',
+      '#00A368',
+      '#FFD300'
+    ];
+
+    // let blueStyle = solidColoredButtonStyle('#1c6491');
+    // let redStyle = solidColoredButtonStyle('#c1224d');
+    // let greenStyle = solidColoredButtonStyle('#198c16');
+    // let yellowStyle = solidColoredButtonStyle('#fbdf69');
+    // let blackStyle = solidColoredButtonStyle('black');
+    // let grayStyle = solidColoredButtonStyle('gray');
+    // let whiteStyle = solidColoredButtonStyle('white');
+    // let blueStyle = solidColoredButtonStyle('#0088BF');
+    // let redStyle = solidColoredButtonStyle('#C40233');
+    // let greenStyle = solidColoredButtonStyle('#00A368');
+    // let yellowStyle = solidColoredButtonStyle('#FFD300');
     return (
       <Layout style={style.rootContainer}>
         <Layout style={style.nav}>
@@ -148,10 +242,11 @@ class App extends Component {
           </div>
           <div>
             Themes:
+            <Link onClick={this._changeTheme('bw').bind(this)}>B & W</Link>
+            <Link onClick={this._changeTheme('radioactive').bind(this)}>Radioactive</Link>
             <Link onClick={this._changeTheme('terminal').bind(this)}>Terminal</Link>
             <Link onClick={this._changeTheme('ocean').bind(this)}>Ocean</Link>
             <Link onClick={this._changeTheme('mocha').bind(this)}>Mocha</Link>
-            <Link onClick={this._changeTheme('mochaInverted').bind(this)}>Mocha Inverted</Link>
             <Link onClick={this._changeTheme('sunset').bind(this)}>Sunset</Link>
             <Link onClick={this._changeTheme('lime').bind(this)}>Lime</Link>
           </div>
@@ -174,10 +269,16 @@ class App extends Component {
             &nbsp;
             <button style={style.btn({solid: true})}>Submit</button>
             &nbsp;
-            <button style={blueStyle}>Blue</button>
-            <button style={redStyle}>Red</button>
-            <button style={greenStyle}>Green</button>
-            <button style={yellowStyle}>Yellow</button>
+            <hr />
+            {
+              colors.map(color => (
+                <span>
+                  <button style={solidColoredButtonStyle(color, 1)}>Color</button>
+                  &nbsp;
+                </span>
+              ))
+            }
+            <hr />
             <div style={{marginBottom: size(1)}}>
               <span>Change color: </span>
               <input
@@ -213,7 +314,18 @@ class App extends Component {
               <hr />
               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </span>
-            { sizes.map(size => <div style={{background: themeScale(size/10), height: 40}}></div>) }
+            { sizes.map(size =>
+              <div style={{background: themeScale(size/10), padding: 10}}>
+              {
+                colors.map(color =>
+                  <span>
+                    <button style={solidColoredButtonStyle(color, size/10)}>Color</button>
+                    &nbsp;
+                  </span>
+                )
+              }
+              </div>
+            ) }
             {
               sizes.map(size => <div style={{padding: 5, background: sunsetScale(size/10), color: sunsetScale(size/10 - 0.5)}}>{size}</div>)
             }
@@ -229,12 +341,17 @@ class App extends Component {
       </Layout>
     );
   }
-  colorer (opts) {
+  colorer (opts = {invert: false}) {
+    if (this.state.invert) opts.invert = !opts.invert;
     let scale = this.state;
+    let startColor = d3.hcl(scale.startColor);
+    let endColor = d3.hcl(scale.endColor);
+    // startColor = d3.hcl(startColor.h, 0, startColor.l);
+    // endColor = d3.hcl(endColor.h, 0, endColor.l);
     if (opts && opts.invert === true) {
-      return scale.interpolator.call(null, scale.endColor, scale.startColor);
+      return scale.interpolator.call(null, endColor, startColor);
     }
-    return scale.interpolator.call(null, scale.startColor, scale.endColor);
+    return scale.interpolator.call(null, startColor, endColor);
   }
   styler () {
     var themeScale = this.colorer();
